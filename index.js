739e595b4fb4acd9355c004bdc184d932a958902
Ossi Herrala <oherrala@iki.fi>
2016-11-04 14:04:08 +0200

    Good enough version for now
    nat_cracker() is a promise with configurable timeout and WebRTC
    teardown after enough tries has been made. Calls next promise only
    after all results are collected.

--- index.js
+++ index.js
@@ -1,29 +1,86 @@
-natCracker = function(callback) {
-    unique_ips = new Set();
-    iceServers = { "iceServers": [
-        { "urls": "stun:stun.l.google.com:19302" },
-        { "urls": "stun:stun.services.mozilla.com" }
-    ]};
+nat_cracker = function(timeout) {
+    if(!timeout) {
+        timeout = 1000;
+    }
 
-    peerConnection = new RTCPeerConnection(iceServers);
+    tear_down = function(pc, resolve) {
+        pc.close();
+        resolve(results);
+    }
 
-    peerConnection.onicecandidate = function (event) {
-        if (event.candidate) {
-            /* FIXME: Cutting corners here */
+    nat_cracker_promise = function(resolve, reject) {
+        // Set of unique IPs found
+        unique_ips = new Set();
+
+        // Array of results
+        results = new Array();
+
+        // Count onicecandidate callbacks
+        callbacks_done = 0;
+
+        iceServers = {
+            "iceCandidatePoolSize": 64,
+            "iceServers": [
+                { "urls": "stun:stun.l.google.com:19302" },
+                { "urls": "stun:stun.services.mozilla.com" }
+            ]
+        };
+
+        peerConnection = new RTCPeerConnection(iceServers);
+
+        close_timer_id = window.setTimeout(
+            tear_down,
+            timeout,
+            peerConnection,
+            resolve
+        );
+
+        peerConnection.onicecandidate = function (event) {
+            callbacks_done++;
+
+            // https://tools.ietf.org/html/rfc5245#section-15.1
             parts = event.candidate.candidate.split(" ");
+
+            // We don't know yet what kind of IP this is. Maybe it's browser's
+            // IP or public IP of NAT gw
             ip = parts[4];
 
-            if(!unique_ips.has(ip)) {
-                unique_ips.add(ip);
-                callback(parts[4]);
+            type = null;
+            if (parts.indexOf("typ")) {
+                type = parts[parts.indexOf("typ")+1];
             }
-        }
-    };
 
-    var options = { offerToReceiveVideo: true, offerToReceiveAudio: true };
-    peerConnection.createOffer(options).then(
-        function (localSessionDescription) {
-            peerConnection.setLocalDescription(localSessionDescription);
-        }
-    );
+            raddr = null;
+            if(type && type.startsWith("srflx")) {
+                // Server reflexive address
+                raddr = parts[parts.indexOf("raddr")+1];
+                type = "nat";
+            }
+
+            if(ip && !unique_ips.has(ip)) {
+                unique_ips.add(ip);
+                results.push({ "ip": ip, "type": type });
+            }
+
+            if(raddr && !unique_ips.has(raddr)) {
+                unique_ips.add(ip);
+                results.push({ "ip": raddr, "type": "host" });
+            }
+
+            if(callbacks_done / unique_ips.size > 4) {
+                window.clearTimeout(close_timer_id);
+                tear_down(peerConnection, resolve);
+            }
+
+        };
+
+        var options = { offerToReceiveVideo: true, offerToReceiveAudio: true };
+        peerConnection.createOffer(options).then(
+            function (localSessionDescription) {
+                peerConnection.setLocalDescription(localSessionDescription);
+            }
+        );
+    }
+
+    return new Promise(nat_cracker_promise);
 }
